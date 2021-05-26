48faa1a2ffe30843198db87a3f30ba811b5659f1
mbottion <84838458+mbottion@users.noreply.github.com>
2021-05-26 14:49:25 +0200

    Update getMain.sh

--- getMain.sh
+++ getMain.sh
@@ -5,5 +5,5 @@ die()
   exit 1
 }
 SCRIPT=getMain.sh
-[ "$1" = "" ] && die "$1 not understood, Please enter the script Type [runSQL|runShell]"
+[ "$1" = "" ] && die "$1 not understood, P lease enter the script Type [runSQL|runShell]"
 echo "Getting Main scripts : $1"
