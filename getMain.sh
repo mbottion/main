767ecf402ae659d8825d298edada9e819ae98016
mbottion <84838458+mbottion@users.noreply.github.com>
2021-05-26 14:15:03 +0200

    Update getMain.sh

--- getMain.sh
+++ getMain.sh
@@ -4,5 +4,6 @@ die()
   [ "$1" != "" ] && echo -e "$SCRIPT : Error \n$*"
   exit 1
 }
+SCRIPT=$(basename $0)
 [ "$1" = "" ] && die "Please enter the script Type [runSQL|runShell]"
 echo "Getting Main scripts : $1"
