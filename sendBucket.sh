d5ce98b0b78709250b9a4c0f1bab9dce98aa27a4
mbottion <84838458+mbottion@users.noreply.github.com>
2021-09-25 19:32:32 +0200

    Updated by runSQL.sh

--- sendBucket.sh
+++ sendBucket.sh
@@ -27,7 +27,17 @@ do
     ?|h) shift ; usage ;;
   esac
 done
-
+if [ "$OCI_VAR_OK" != "Y" ]
+then
+  #
+  #    Vars were set-up in the calling Script
+  #
+  OCICLI=""
+  testFile="/admindb/ocicli/bin/oci" ; test -f $testFile && OCICLI=$testFile
+  scriptFile=$(readlink -f $0)
+  OCICONFIG=$(dirname $scriptFile)/.oci/config
+  test -f $OCICONFIG || OCICLI=""
+fi
 
 [ "$1" = "" ] && die "No file to send"
 
@@ -49,8 +59,30 @@ do
       echo "   |"
     else
       echo "OK"
+      f2=$(basename $f)
+      if [ "$paRequest" != "N" ]
+      then
+        if [ "$OCICLI" != "" ]
+        then
+          echo "    - Generating Pre-authenticated Request ...."
+          expireDate=$(date -d "Tomorrow" "+%Y-%m-%dT%H:%M:%SZ")
+          result=$($OCICLI os preauth-request create --config-file $OCICONFIG --access-type ObjectRead --bucket-name MBO --object-name $f2 --time-expires $expireDate --name $f2)
+          if [ $? -eq 0 ]
+          then
+            accessURI=$(echo "$result" | grep access-uri | cut -f2 -d":"| cut -f2 -d "\"")
+            echo "    - File can be downloaded with "
+            echo
+            echo "curl -O https://objectstorage.eu-frankfurt-1.oraclecloud.com$accessURI"  | fold -w 90 | sed -e "s;$;\\\\;" | sed -e "$ s;\\\\$;;"
+            echo
+          else
+            echo "    - Access URI not generated"
+          fi
+        else
+          echo "Unable to find ocicli or config file ($OCICONFIG)"
+        fi
+      fi
+      rm -f /tmp/$$.tmp
     fi
-    rm -f /tmp/$$.tmp
   else
     echo "File not found"
   fi
