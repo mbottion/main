a0ac2b5a8a032233a8bb7c883c8255ee7399aea1
mbottion <84838458+mbottion@users.noreply.github.com>
2023-05-24 18:11:06 +0200

    Create shellTemplate.sh

---
+++ shellTemplate.sh
@@ -0,0 +1,218 @@
+VERSION=0.1
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+#   Appelé par l'option -T, permet de tester des parties de script
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+testUnit()
+{
+  startStep "Test de fonctionnalites"
+
+  endStep
+}
+showVars()
+{
+  echo
+  echo "==============================================================="
+  echo "Variables : "
+  echo "==============================================================="
+  echo "  - TNS_ADMIN        : $TNS_ADMIN"
+  echo
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#     Trace
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+startRun()
+{
+  START_INTERM_EPOCH=$(date +%s)
+  echo   "========================================================================================" 
+  echo   " Execution start"
+  echo   "========================================================================================" 
+  echo   "  - $1"
+  echo   "  - Started at     : $(date)"
+  echo   "========================================================================================" 
+  echo
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#     Trace
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+endRun()
+{
+  END_INTERM_EPOCH=$(date +%s)
+  all_secs2=$(expr $END_INTERM_EPOCH - $START_INTERM_EPOCH)
+  mins2=$(expr $all_secs2 / 60)
+  secs2=$(expr $all_secs2 % 60 | awk '{printf("%02d",$0)}')
+  echo
+  echo   "========================================================================================" 
+  echo   "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+  echo   "  - Ended at      : $(date)" 
+  echo   "  - Duration      : ${mins2}:${secs2}"
+  echo   "========================================================================================" 
+  echo   "Script LOG in : $LOG_FILE"
+  echo   "========================================================================================" 
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#     Trace
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+startStep()
+{
+  STEP="$1"
+  STEP_START_EPOCH=$(date +%s)
+  echo
+  echo "       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+  echo "       - Step (start)  : $STEP"
+  echo "       - Started at    : $(date)" 
+  echo "       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+  echo
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#     Trace
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+endStep()
+{
+  STEP_END_EPOCH=$(date +%s)
+  all_secs2=$(expr $STEP_END_EPOCH - $STEP_START_EPOCH)
+  mins2=$(expr $all_secs2 / 60)
+  secs2=$(expr $all_secs2 % 60 | awk '{printf("%02d",$0)}')
+  echo
+  echo "       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+  echo "       - Step (end)    : $STEP"
+  echo "       - Ended at      : $(date)" 
+  echo "       - Duration      : ${mins2}:${secs2}"
+  echo "       - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#     Abort du programme
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+die() 
+{
+  endRun
+  echo "
+ERROR :
+  $*"
+
+  exit 1
+}
+
+exec_sql()
+{
+#
+#  Don't forget to use : set -o pipefail un the main program to have error managempent
+#
+  local VERBOSE=N
+  local SILENT=N
+  if [ "$1" = "-silent" ]
+  then 
+    SILENT=Y
+    shift
+  fi
+  if [ "$1" = "-no_error" ]
+  then
+    err_mgmt="whenever sqlerror continue"
+    shift
+  else
+    err_mgmt="whenever sqlerror exit failure"
+  fi
+  if [ "$1" = "-verbose" ]
+  then
+    VERBOSE=Y
+    shift
+  fi
+  local login="$1"
+  local stmt="$2"
+  local lib="$3"
+  local bloc_sql="$err_mgmt
+set recsep off
+set head off 
+set feed off
+set pages 0
+set lines 2000
+connect ${login}
+$stmt"
+  REDIR_FILE=""
+  REDIR_FILE=$(mktemp)
+  if [ "$lib" != "" ] 
+  then
+     printf "%-75s : " "$lib";
+     sqlplus -s /nolog >$REDIR_FILE 2>&1 <<%EOF%
+$bloc_sql
+%EOF%
+    status=$?
+  else
+     sqlplus -s /nolog <<%EOF% | tee $REDIR_FILE  
+$bloc_sql
+%EOF%
+    status=$?
+  fi
+  if [ $status -eq 0 -a "$(egrep "SP2-" $REDIR_FILE)" != "" ]
+  then
+    status=1
+  fi
+  if [ "$lib" != "" ]
+  then
+    [ $status -ne 0 ] && { echo "*** ERREUR ***" ; test -f $REDIR_FILE && cat $REDIR_FILE ; rm -f $REDIR_FILE ; } \
+                      || { echo "OK" ; [ "$VERBOSE" = "Y" ] && test -f $REDIR_FILE && sed -e "s;^;    > ;" $REDIR_FILE ; }
+  fi 
+  rm -f $REDIR_FILE
+  [ $status -ne 0 ] && return 1
+  return $status
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#  Usage
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+usage() 
+{
+ echo "$SCRIPT : $SCRIPT_LIB
+
+Usage :
+ $SCRIPT 
+         [-h|-?]
+
+      Description of the script
+
+         -?|-h        : Help
+
+  Version : $VERSION
+  "
+  exit
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+set -o pipefail
+
+SCRIPT=shellTemplate.sh
+SCRIPT_LIB="Basic shell template"
+
+#[ "$(id -un)" != "oracle" ] && die "Merci de lancer ce script depuis l'utilisateur \"oracle\""
+#[ "$(hostname -s | sed -e "s;.*\([0-9]\)$;\1;")" != "1" ] && die "Lancer ce script depuis le premier noeud du cluster"
+
+#[ "$1" = "" ] && usage
+
+toShift=0
+while getopts h opt
+do
+  case $opt in
+   # --------- Source Database --------------------------------
+   # --------- Target Database --------------------------------
+   # --------- Modes de fonctionnement ------------------------
+   # --------- Usage ------------------------------------------
+   ?|h) usage "Help requested";;
+  esac
+done
+shift $toShift 
+# -----------------------------------------------------------------------------
+#
+#       Analyse des paramètres et valeurs par défaut
+#
+# -----------------------------------------------------------------------------
+
+LOG_FILE=/dev/null
+
+startRun "Shell Template"
+
+die
+
+endRun
