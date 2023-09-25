7748567a342b8f3e8caa92d7c550ca0025390532
mbottion <84838458+mbottion@users.noreply.github.com>
2023-09-25 09:39:33 +0200

    Updated by runScript.sh

--- sendBucket.sh
+++ sendBucket.sh
@@ -329,10 +329,10 @@ MAX_SIZE=$((1024 * 1024 * 1024 * 20))           # Beyond this size, we use multi
 # MAX_SIZE=0                                    # If MAX_SIZE=0, standard upload, whatever the file size is
 SPLIT_SIZE=$((1024 * 1024 * 1024 * 1))          # Split chunck size
 MAX_CURL=20                                     # Number of cuncurrent curls permited
-genPAR=Y                                        # After upload, a attempt to create a PAR is done
-# PAR to write to the bucket
+genPAR=N                                        # After upload, a attempt to create a PAR is done
+# PAR to write to the bucket (PAR wite : expires 2024-12-31)
 bucketName=
-# PAR to read the bucket
+# PAR to read the bucket (PAR read : expires 2024-12-31)
 bucketRead=
 
 trap killSplit INT                              # Trap to clean background processes 
